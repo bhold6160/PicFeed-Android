a3329f51907889d8133cc3f1452ba5296e134f82
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-10-07 16:05:39 -0700

    camera button crashing

---
+++ app/src/main/java/com/example/brandonholderman/picfeed/ImageAdapter.java
@@ -0,0 +1,41 @@
+package com.example.brandonholderman.picfeed;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by brandonholderman on 10/7/17.
+ */
+
+public class ImageAdapter extends ArrayAdapter<String> {
+    Context mContext;
+    List<String> mUrls;
+    int mLayoutId;
+
+    public ImageAdapter(Context context, int resource, List<String> urls) {
+        super(context, resource, urls);
+        mContext = context;
+        mLayoutId = resource;
+        mUrls = urls;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(mLayoutId, parent, false);
+        }
+
+        ImageView image = convertView.findViewById(R.id.download_picture);
+        String url = mUrls.get(position);
+
+//        (new DownloadImageTask(mContext, url, image)).execute();
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/brandonholderman/picfeed/MainActivity.java
+++ app/src/main/java/com/example/brandonholderman/picfeed/MainActivity.java
@@ -13,24 +13,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private final int REQUEST_IMAGE_CAPTURE =1;
     private final int REQUEST_PICK_PICTURE = 2;
 
     private Button mTakePictureButton;
     private Button mPickPictureButton;
+    private Button mDownloadPictureButton;
     private ImageView mImageResult;
     private String mCurrentPhotoPath;
     private TextView mInfo;
+
+    private ListView mImageList;
+    private ImageAdapter mAdapter;
+    private List<String> mImageUrls;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +51,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mImageResult = (ImageView) findViewById(R.id.image_result);
-        mInfo = (TextView) findViewById(R.id.error_text);
+        mInfo = (TextView) findViewById(R.id.info);
         mTakePictureButton = (Button) findViewById(R.id.take_picture);
+        mPickPictureButton = (Button) findViewById(R.id.pick_picture);
+        mDownloadPictureButton = (Button) findViewById(R.id.download_picture);
+
+        mImageUrls = MockDatabase.allImages;
+        mImageList = (ListView) findViewById(R.id.imageList);
+        mAdapter = new ImageAdapter(this, R.layout.image_item, mImageUrls);
+        mImageList.setAdapter(mAdapter)
 
         attachClickHandlers();
     }
@@ -48,8 +68,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        if(resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE) {
 
-        setPictureFromFile();
+            setPictureFromFile();
+            addPicToGallery();
+        } else if (resultCode == RESULT_OK && requestCode == REQUEST_PICK_PICTURE) {
+            setPictureFromGallery(data);
+        }
+
 
 //        Bundle extras = data.getExtras();
 //        Bitmap imageBitmap = (Bitmap) extras.get("data");
@@ -57,33 +83,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void attachClickHandlers() {
-        mTakePictureButton = (Button) findViewById(R.id.take_picture);
         mTakePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 takePicture();
             }
         });
+
+        mPickPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickPicture();
+            }
+        });
+
+        mDownloadPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadPicture();
+            }
+        });
     }
 
     private void takePicture() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 0);
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(intent, 0);
 
             try {
                 File imageFile = createImageFile();
                 Uri photoUri = FileProvider.getUriForFile(this, "com.example.brandonholderman.picfeed", imageFile);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 getIntent().putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
 
-
+                startActivityForResult(intent, 1);
             } catch(IOException e) {
                 String error = "Error: " + e.getMessage();
                 Log.d(TAG, "onCreate: " + error);
                 mInfo.setText(error);
             }
         }
-    }
+
 
     private void pickPicture() {
         Intent intent = new Intent();
@@ -93,9 +133,12 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_PICK_PICTURE);
     }
 
+    private void downloadPicture() {
+    }
+
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String fileName = "JPEG " + timeStamp + " ";
+        String fileName = "JPEG_" + timeStamp + "_";
 
         File directory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File imageFile = File.createTempFile(fileName, ".jpg", directory);
@@ -105,8 +148,26 @@ public class MainActivity extends AppCompatActivity {
         return imageFile;
     }
 
+    private void addPicToGallery() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
     private void setPictureFromFile() {
         Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
         mImageResult.setImageBitmap(bitmap);
+    }
+
+    private void setPictureFromGallery(Intent data) {
+        try {
+            InputStream stream = this.getContentResolver().openInputStream(data.getData());
+            Bitmap galleryPicture = BitmapFactory.decodeStream(stream);
+            mImageResult.setImageBitmap(galleryPicture);
+        } catch(FileNotFoundException e) {
+            mInfo.setText("Error: " + e.getMessage());
+        }
     }
 }
